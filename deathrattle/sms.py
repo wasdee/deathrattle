e7e719cc2ab21cd75468746455cc7f04f904c616
Nutchanon Ninyawee <me@nutchanon.org>
2019-10-23 09:35:58 +0700

    Update sms.py

--- deathrattle/sms.py
+++ deathrattle/sms.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC04317ad3070ade6116edca682096209e"
+account_sid = ""
 # Your Auth Token from twilio.com/console
-auth_token  = "824e5400c8380c7844ab0bdf708aed41"
+auth_token  = ""
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+66992866936", 
-    from_=''.join("+1 240 743 2139".split()),
-    body="Hello from Python!")
+    to="", 
+    from_=''),
+    body="")
